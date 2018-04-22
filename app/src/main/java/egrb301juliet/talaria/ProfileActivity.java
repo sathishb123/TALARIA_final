66c2c2e879691314e39c925cc721299b0d6a1570
sathishb123 <31788083+sathishb123@users.noreply.github.com>
2018-04-21 23:57:03 -0400

    Initial commit - minor changes

---
+++ app/src/main/java/egrb301juliet/talaria/ProfileActivity.java
@@ -0,0 +1,19 @@
+package egrb301juliet.talaria;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
